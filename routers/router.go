d3c3714fa6a8645d3a8e45b233634bd420994b60
yuwei0713 <ads9210155@gmail.com>
2024-09-24 16:00:12 +0800

    Router Fix

--- routers/router.go
+++ routers/router.go
@@ -21,69 +21,66 @@ func InitRouters() *gin.Engine {
 	store := cookie.NewStore([]byte("secret"))
 	Router.Use(sessions.Sessions("auth-session", store))
 
-	AllRouter := Router.Group("/api")
-	{
-		AllRouter.POST("/Login", controllers.Login)
-		AllRouter.POST("/Register", controllers.Register)
-		// Router.POST("/Logout")
+	Router.POST("/Login", controllers.Login)
+	Router.POST("/Register", controllers.Register)
+	// Router.POST("/Logout")
 
-		DashBoardRoute := AllRouter.Group("/")
+	DashBoardRoute := Router.Group("/")
+	{
+		DashBoardRoute.GET("/", func(c *gin.Context) {
+			c.String(200, "test")
+		})
+		QuestionManageRoute := Router.Group("/QuestionManage")
 		{
-			DashBoardRoute.GET("/", func(c *gin.Context) {
-				c.String(200, "test")
-			})
-			QuestionManageRoute := AllRouter.Group("/QuestionManage")
+			QuestionManageRoute.GET("/", controllers.ShowQuestionnaire())
+			QuestionManageRoute.POST("/QuestionDetail", controllers.ShowDetailQuestionnaire())
+		}
+		SearchRouter := Router.Group("/Search")
+		{
+			SearchTeacherRoute := SearchRouter.Group("/Teacher")
 			{
-				QuestionManageRoute.GET("/", controllers.ShowQuestionnaire())
-				QuestionManageRoute.POST("/QuestionDetail", controllers.ShowDetailQuestionnaire())
+				SearchTeacherRoute.GET("/", controllers.ShowSearch("Teacher", "0"))
+				SearchTeacherRoute.POST("/user", controllers.ShowDetail("Teacher", "0"))
 			}
-			SearchRouter := AllRouter.Group("/Search")
+			SearchChildrenRoute := SearchRouter.Group("/Children")
 			{
-				SearchTeacherRoute := SearchRouter.Group("/Teacher")
-				{
-					SearchTeacherRoute.GET("/", controllers.ShowSearch("Teacher", "0"))
-					SearchTeacherRoute.POST("/user", controllers.ShowDetail("Teacher", "0"))
-				}
-				SearchChildrenRoute := SearchRouter.Group("/Children")
-				{
-					SearchChildrenRoute.GET("/", controllers.ShowSearch("Children", "0"))
-					SearchChildrenRoute.POST("/Child", controllers.ShowDetail("Children", "0"))
-					SearchChildrenRoute.POST("/Update", controllers.ChildManage("Update", "0"))
-				}
-				SearchFillStatusRoute := SearchRouter.Group("/FillStatus")
-				{
-					SearchFillStatusRoute.GET("/", controllers.ShowSearch("FillStatus", "0"))
-					SearchFillStatusRoute.POST("/DetailData", controllers.ShowDetail("FillResult", "0"))
-				}
-				DataExportRoute := AllRouter.Group("/DataExport")
-				{
-					DataExportRoute.GET("", controllers.ExportToExcel())
-					// CustomizeExportRoute := Router.Group("/Customize")
-					// {
-					// 	CustomizeExportRoute.GET("/")
-					// 	CustomizeExportRoute.POST("/")
-					// }
-				}
+				SearchChildrenRoute.GET("/", controllers.ShowSearch("Children", "0"))
+				SearchChildrenRoute.POST("/Child", controllers.ShowDetail("Children", "0"))
+				SearchChildrenRoute.POST("/Update", controllers.ChildManage("Update", "0"))
 			}
-			ManageRouter := AllRouter.Group("/Manage")
+			SearchFillStatusRoute := SearchRouter.Group("/FillStatus")
 			{
-				UserManageRoute := ManageRouter.Group("/Users")
-				{
-					UserManageRoute.GET("/", controllers.UserManage("Search", "0"))
-					UserManageRoute.POST("/UserInsert", controllers.ClaMEISR_Register)
-					UserManageRoute.POST("/UserUpdate", controllers.UserManage("Update", "0"))
-					// UserManageRoute.POST("/UserDelete")
-				}
-				SchoolManageRoute := ManageRouter.Group("/Schools")
-				{
-					SchoolManageRoute.GET("/", controllers.SchoolManage("Search", "0"))
-					SchoolManageRoute.POST("/SchoolInsert", controllers.SchoolManage("Insert", "0"))
-					// SchoolManageRoute.POST("/SchoolUpdate")
-					// SchoolManageRoute.POST("/SchoolDelete")
-					SchoolManageRoute.POST("/ClassInsert", controllers.SchoolManage("Insert", "0"))
-					SchoolManageRoute.POST("/ClassUpdate", controllers.SchoolManage("Update", "0"))
-					SchoolManageRoute.POST("/ClassDelete", controllers.SchoolManage("Delete", "0"))
-				}
+				SearchFillStatusRoute.GET("/", controllers.ShowSearch("FillStatus", "0"))
+				SearchFillStatusRoute.POST("/DetailData", controllers.ShowDetail("FillResult", "0"))
+			}
+			DataExportRoute := Router.Group("/DataExport")
+			{
+				DataExportRoute.GET("", controllers.ExportToExcel())
+				// CustomizeExportRoute := Router.Group("/Customize")
+				// {
+				// 	CustomizeExportRoute.GET("/")
+				// 	CustomizeExportRoute.POST("/")
+				// }
+			}
+		}
+		ManageRouter := Router.Group("/Manage")
+		{
+			UserManageRoute := ManageRouter.Group("/Users")
+			{
+				UserManageRoute.GET("/", controllers.UserManage("Search", "0"))
+				UserManageRoute.POST("/UserInsert", controllers.ClaMEISR_Register)
+				UserManageRoute.POST("/UserUpdate", controllers.UserManage("Update", "0"))
+				// UserManageRoute.POST("/UserDelete")
+			}
+			SchoolManageRoute := ManageRouter.Group("/Schools")
+			{
+				SchoolManageRoute.GET("/", controllers.SchoolManage("Search", "0"))
+				SchoolManageRoute.POST("/SchoolInsert", controllers.SchoolManage("Insert", "0"))
+				// SchoolManageRoute.POST("/SchoolUpdate")
+				// SchoolManageRoute.POST("/SchoolDelete")
+				SchoolManageRoute.POST("/ClassInsert", controllers.SchoolManage("Insert", "0"))
+				SchoolManageRoute.POST("/ClassUpdate", controllers.SchoolManage("Update", "0"))
+				SchoolManageRoute.POST("/ClassDelete", controllers.SchoolManage("Delete", "0"))
 			}
 		}
 	}
